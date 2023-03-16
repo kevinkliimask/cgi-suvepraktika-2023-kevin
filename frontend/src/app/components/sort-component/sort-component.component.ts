4d5752c85a9b5c84473c385ccbba5849c175b9de
Kevin Kliimask <kevin.kliimask@ut.ee>
2023-03-16 14:08:18 +0200

    Add untracked files

---
+++ frontend/src/app/components/sort-component/sort-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sort-component',
+  templateUrl: './sort-component.component.html',
+  styleUrls: ['./sort-component.component.css']
+})
+export class SortComponentComponent {
+
+}
